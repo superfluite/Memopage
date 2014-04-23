4b555217406c3b77dcf307c00263a9f141c234ed
superfluite <superfluite_@naver.com>
2014-04-23 15:57:22 +0900

    fix bug

--- memopage.py
+++ memopage.py
@@ -112,8 +112,8 @@ def withdraw():
 @app.route('/delaccount')
 @login_required
 def delaccount():
-	wtf=db_session.query(Memo).filter_by(writerid=current_user.id)
-	db_session.delete(wtf)
+	poor_memo=db_session.query(Memo).filter_by(writerid=current_user.id).all()
+	db_session.deletes(poor_memo)
 	poor_user=db_session.query(User).filter_by(id=current_user.id).first()
 	session.pop('logged_in',None)
 	logout_user()
